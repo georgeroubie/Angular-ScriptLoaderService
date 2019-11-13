4c150140514c96a0edd7de2b050bd8e77069af2c
George Roubie <george.roubie@gmail.com>
2019-11-13 19:41:40 +0200

    Update script-loader.service.ts

--- script-loader.service.ts
+++ script-loader.service.ts
@@ -3,48 +3,38 @@ import { Injectable } from '@angular/core';
 // Application Interfaces
 import { ScriptModel } from 'script-model';
 // RxJs
-import { Observable } from 'rxjs';
-import { Observer } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable()
 export class ScriptLoaderService {
-    private scripts: ScriptModel[] = [];
+	private scripts: IScriptModel[] = [];
 
-    public load(script: ScriptModel, nocache: boolean = false): Observable<ScriptModel> {
-        return new Observable<ScriptModel>((observer: Observer<ScriptModel>) => {
-            let existingScript = this.scripts.find(s => s.name == script.name);
-
-            // Complete if already loaded
-            if (existingScript && existingScript.loaded) {
-                observer.next(existingScript);
-                observer.complete();
-            }
-            else {
-                // Add the script
-                this.scripts = [...this.scripts, script];
-
-                // Load the script
-                let scriptElement = document.createElement('script');
-                scriptElement.type = 'text/javascript';
-                if(nocache) {
-                    scriptElement.src = `${script.src}?v=${Date.now()}`;
-                } else {
-                    scriptElement.src = script.src;
-                }
-
-                scriptElement.onload = () => {
-                    script.loaded = true;
-                    observer.next(script);
-                    observer.complete();
-                };
-
-                scriptElement.onerror = (error: any) => {
-                    console.log(`Couldn't load script ${script.src}`);
-                    observer.error(error);
-                };
-
-                document.getElementsByTagName('body')[0].appendChild(scriptElement);
-            }
-        });
-    }
+	public load(script: IScriptModel, nocache: boolean = false): Observable<IScriptModel> {
+		return new Observable<IScriptModel>((observer: Observer<IScriptModel>) => {
+			const existingScript: IScriptModel = this.scripts.find((s) => s.name === script.name);
+			// Complete if the script is already loaded
+			if (existingScript && existingScript.loaded) {
+				observer.next(existingScript);
+				observer.complete();
+			} else {
+				// Add the script to scripts array
+				this.scripts = [...this.scripts, script];
+				// Create the script
+				const scriptElement: HTMLScriptElement = document.createElement('script');
+				scriptElement.type = 'text/javascript';
+				scriptElement.src = nocache ? `${script.src}?v=${Date.now()}` : script.src;
+				scriptElement.onload = () => {
+					script.loaded = true;
+					observer.next(script);
+					observer.complete();
+				};
+				scriptElement.onerror = (error: string | Event) => {
+					console.log(`Couldn't load script ${script.src}`);
+					observer.error(error);
+				};
+				// Load the script
+				document.getElementsByTagName('body')[0].appendChild(scriptElement);
+			}
+		});
+	}
 }
